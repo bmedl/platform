d4f78c3eaf858906fa5eb6da740580fd09b55215
tamasf97 <tamasf97@outlook.com>
2019-12-15 23:53:43 +0100

    added docs to models

--- backend/stocks/stocks/models.py
+++ backend/stocks/stocks/models.py
@@ -5,6 +5,9 @@ from django.utils.encoding import python_2_unicode_compatible
 
 # https://developer.oanda.com/rest-live-v20/pricing-df/
 class Stock(models.Model):
+    """
+    Various data describing a stock.
+    """
     name = models.CharField(max_length=120)
     event_date = models.DateTimeField(auto_now=True)
     price_date = models.DateTimeField()
@@ -17,14 +20,19 @@ class Stock(models.Model):
     tradeable = models.BooleanField()
 
 
-# A trained Keras model
 class NetworkModel(models.Model):
+    """
+    A trained Keras model saved as a blob in hdf5 format.
+    """
     name = models.CharField(max_length=120, blank=True, default='')
     updated = models.DateTimeField(auto_now=True)
     model_blob = models.BinaryField()
 
 
 class Prediction(models.Model):
+    """
+    A predicted value.
+    """
     created = models.DateTimeField(auto_now=True)
     time_range = models.DurationField()
     price_date = models.DateTimeField()
