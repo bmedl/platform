8ef7cf3a1a6c8f2493b1d8d634ce90b1d1daec63
tamasf97 <tamasf97@outlook.com>
2020-01-08 02:15:55 +0100

    added import hack

--- backend/stocks/stocks/viewsets.py
+++ backend/stocks/stocks/viewsets.py
@@ -25,7 +25,11 @@ class Last100Stocks(ModelViewSet):
 class Predict(ViewSet):
     @action(detail=True, methods=['post'])
     def predict(self, request, pk=None):
-        from ..neuralnet.run import predict as predict_fn
+        from os import sys
+        sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+        from neuralnet.run import predict as predict_fn
+
         try:
             data = request.data
             predict_fn(data['name'])
